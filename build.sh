0e7953be8edc78c90b77facddbf97a353f6da96f
Bright Davor <brightkofidavor@email.com>
2026-03-28 03:26:12 +0000

    Fix build.sh merge conflict

--- build.sh
+++ build.sh
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-# Create a proper build.sh file
-@'
-#!/bin/bash
-set -e
-
-echo "Installing dependencies..."
-pip install -r requirements.txt
-
-echo "Running migrations..."
-python manage.py migrate
-
-echo "Collecting static files..."
-python manage.py collectstatic --noinput
-
-echo "Build completed!"
-'@ | Out-File -FilePath build.sh -Encoding UTF8
-=======
 #!/usr/bin/env bash
 
 echo "Starting build..."
@@ -26,4 +8,3 @@ python manage.py collectstatic --noinput
 python manage.py migrate
 
 echo "Build completed!"
->>>>>>> 2a65adf (Clean project (no large files))
